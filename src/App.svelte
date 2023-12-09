571a772d4c0d81412abf1c6cccf6c2e5002a788e
ChessScholar <matthew.kolcz@gmail.com>
2023-12-09 17:16:35 -0500

    Signed-off-by: ChessScholar <matthew.kolcz@gmail.com>

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+ import ComfyApp from "$lib/components/ComfyApp.svelte"
+ import { default as ComfyAppState } from "$lib/components/ComfyApp"
+ import "@litegraph-ts/core/css/litegraph.css";
+ import "./scss/global.scss";
+
+ export let app: ComfyAppState;
+ export let isMobile: boolean
+</script>
+
+{#if isMobile}
+    <div>Redirecting...</div>
+{:else}
+    <ComfyApp {app}/>
+{/if}
